77a766a0e9d99a8fd080c688af16729be1d021d4
runArun <zl2613185@qq.com>
2017-11-09 15:05:22 +1100

    You added the Angular router to navigate among different components.
    You learned how to create router links to represent navigation menu items.
    
    You used router link parameters to navigate to the details of the user-selected hero.
    
    You shared the HeroService among multiple components.
    
    You added the uppercase pipe to format data.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { HeroService }         from './hero.service';
 
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { DashboardComponent} from "./dashboard.component";
 
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent//declarations数组包含应用中属于该模
+    HeroDetailComponent,//declarations数组包含应用中属于该模
     // 块的组件、管道和指令的列表。 组件在被其它组件引用之前必须先在一个模块中声明过。 这个模块只声明了两个组件：AppComponent 和 HeroDetailComponent。
-
+    HeroesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
-
+    FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    HeroService //添加HeroService到AppModule的providers数组中，因为我们的每一个视图都需要它。
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,22 +1,40 @@
-import { Component,Input} from '@angular/core';
-import { Hero } from './hero';
+// Keep the Input import for now, you'll remove it later:
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location }                 from '@angular/common';
+
+import { HeroService } from './hero.service';
+import { Hero } from "./hero";
+
+import 'rxjs/add/operator/switchMap';
+
 
 @Component({
   selector: 'hero-detail',
-  template:
-      `
-            <div *ngIf="hero">
-              <h2>{{hero.name}} details!</h2>
-              <div><label>id: </label>{{hero.id}}</div>
-              <div>
-                <label>name: </label>
-                <input [(ngModel)]="hero.name" placeholder="name"/>
-              </div>
-            </div>
-      `
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+
 
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap
+      .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))//The hero id is a number. Route parameters are always strings. So the route parameter value is converted to a number with the JavaScript (+) operator.
+
+
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
   @Input() hero: Hero; //通过在hero属性前面加上@Input装饰器，来表明它是一个输入属性。
 }
 
